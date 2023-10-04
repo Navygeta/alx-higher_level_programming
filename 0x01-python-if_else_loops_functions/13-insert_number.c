07e48ea54a56cb2cb0ad4cf52176f41aec3c06d8
Navygeta~ <tinashenavonika@gmail.com>
2023-10-04 05:00:03 +0200

    insert num4

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -5,34 +5,27 @@
  * @number: number to insert into node
  * Return: address of node or NULL on failure
  */
-listint_t *insert_node(listint_t **head, int number);
+listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *nu_node = *current, *prev;
+	listint_t *node = *head, *nu_node;
 
 	nu_node = malloc(sizeof(listint_t));
 	if (nu_node == NULL)
-	{
 		return (NULL);
-	}
 	nu_node->n = number;
-	nu_node->next = NULL;
-	if (*head == NULL || (*head) ->n >= number)
+
+	if (node == NULL || node->n >= number)
 	{
-		nu_node->next = *head;
+		nu_node->next = node;
 		*head = nu_node;
 		return (nu_node);
 	}
-	current = *head;
-	prev = NULL;
 
-	while(current && current->n < number)
-	{
-		prev = current;
-		current = current->next;
-	}
+	while (node && node->next && node->next->n < number)
+		node = node->next;
 
-	nu_node->next = current;
-	prev->next = nu_node;
+	nu_node->next = node->next;
+	node->next = nu_node;
 
 	return (nu_node);
 }
