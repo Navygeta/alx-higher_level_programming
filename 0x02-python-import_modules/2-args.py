7f8909ef12218c4eaedf5d6c6d6eea53a7249d40
Navygeta~ <tinashenavonika@gmail.com>
2023-10-05 22:49:15 +0200

    args2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
 
     if argc == 0:
-        print("0 arguments")
+        print("0 arguments.")
     elif argc == 1:
-        print("1 argument")
+        print("1 argument:")
     else:
         print("{} arguments:".format(argc))
 
