7c9dd40635499e4d245a268f25ef0552af29bc2b
Navygeta~ <tinashenavonika@gmail.com>
2023-10-09 13:23:45 +0200

    element at5

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -7,7 +7,7 @@ Script to retrieve an element from a list.
 
 def element_at(my_list, idx):
 
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
     else:
         return my_list[idx]
