5d64eff26dedd5b2ca3f1498adfff220b0ef055e
Navygeta~ <tinashenavonika@gmail.com>
2023-10-10 01:38:11 +0200

    add tupple5

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -17,5 +17,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     else:
         tuple_b = tuple_b[:2]
 
-    sum_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return(sum_tuple)
+    res_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return(res_tuple)
