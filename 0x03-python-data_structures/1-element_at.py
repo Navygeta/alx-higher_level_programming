ff58dab7e532e6a752be6fda197bd7bd59ce8bc3
Navygeta~ <tinashenavonika@gmail.com>
2023-10-09 13:15:47 +0200

    element at2

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -15,7 +15,7 @@ def element_at(my_list, idx):
     Returns:
         The element at the specified index or None if index is  out of range.
     """
-    if idx < 0 < len(my_list):
-        return my_list[idx]
-    else:
+    if idx < 0 > len(my_list):
         return None
+    else:
+        return my_list[idx]
