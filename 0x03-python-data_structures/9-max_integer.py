b54c11f67b24389dc3a3ffa02d1dfd9c754c17f2
Navygeta~ <tinashenavonika@gmail.com>
2023-10-10 02:06:19 +0200

    max int

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Function that finds the biggest integer of a list.
+"""
+
+
+def max_integer(my_list=[]):
+
+    if not my_list:
+        return None
+
+    max_num = my_list[0]
+
+    for i in range(len(my_list)):
+        if my_list[i] > max_num:
+            max_num = my_list[i]
+    return max_num
