093e8783e4ed5d363e337043971e4255b7d0dbe1
Navygeta~ <tinashenavonika@gmail.com>
2023-10-12 01:41:21 +0200

    mul by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+Function that returns a new dictionary with all values multiplied by 2
+"""
+
+
+def multiply_by_2(a_dictionary):
+
+    new_dict = a_dictionary.items() * 2
+    return new_dict
