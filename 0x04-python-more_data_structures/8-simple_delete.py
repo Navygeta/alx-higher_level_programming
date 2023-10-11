dd8f890855d823578b1eb672d57ffa4127b018e2
Navygeta~ <tinashenavonika@gmail.com>
2023-10-12 01:18:05 +0200

    simple del2

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -7,9 +7,5 @@ Function that deletes a key in a dictionary.
 
 def simple_delete(a_dictionary, key=""):
 
-    if key not in a_dictioanry:
-        pass
-    else:
+    if key in a_dictioanry:
         del a_dictionary[key]
-
-        return a_dictionary
