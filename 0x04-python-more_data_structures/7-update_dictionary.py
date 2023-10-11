f4937c332c329a397eb5f7bae287e4d0e3f39c28
Navygeta~ <tinashenavonika@gmail.com>
2023-10-12 01:05:20 +0200

    update dict1

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -7,8 +7,6 @@ Function that replaces or adds key/value in a dictionary.
 
 def update_dictionary(a_dictionary, key, value):
 
-    if key in a_dictionary:
-        a_dictionary[key] = value
+    a_dictionary[key] = value
 
-    else:
-        a_dictionary[key] = value
+    return a_dictionary
