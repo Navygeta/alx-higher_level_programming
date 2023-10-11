78bb71ea45cf0fa574f444289d010fc6b7d11f86
Navygeta~ <tinashenavonika@gmail.com>
2023-10-12 01:14:09 +0200

    simple del

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Function that deletes a key in a dictionary.
+"""
+
+
+def simple_delete(a_dictionary, key=""):
+
+    if key not in a_dictioanry:
+        pass
+    else:
+        del a_dictionary[key]
