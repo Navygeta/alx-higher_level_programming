2c2ac9e84697e08375def12d2ba636f07a53a75c
Navygeta <tinashenavonika@gmail.com>
2023-10-24 00:52:38 +0200

    safe print2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -13,7 +13,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             elements += 1
         except IndexError:
-            pass
+            break
         finally:
             print("")
     return elements
