25c608c6eb4fbdcd130aa88e43d16ae21ea446f6
Navygeta <tinashenavonika@gmail.com>
2023-10-25 01:27:23 +0200

    2 sqr

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+Module: my_module
+
+This module defines the Square class.
+"""
+
+
+class Square:
+
+    """
+    This class represents a square.
+
+    Attributes:
+        __size: The size of each side of the square.
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a new square instance with given size.
+
+        Args:
+            self: The instance of the square class.
+            size: The size of each side of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
+        elif size < 0:
+            raise ValueError("Size must be >= 0")
+        self.__size = size
