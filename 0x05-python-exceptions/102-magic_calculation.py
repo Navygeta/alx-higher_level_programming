1b37c93000d19e34005cedc0103442518ecf89ba
Navygeta <tinashenavonika@gmail.com>
2023-10-24 03:49:59 +0200

    magic calc

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Magic Calculation
+"""
+
+
+def magic_calculation(a, b):
+
+    solution = 0
+    for i in range (1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            solution += a ** b / i
+        except Exception:
+            solution = b + a
+            break
+    return solution
