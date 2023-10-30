6b45b06f558e10b810d9e7d81cc6ab0a7a6e2d45
Navygeta <tinashenavonika@gmail.com>
2023-10-30 22:55:30 +0200

    rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Module: My module
+
+This module defines a simple rectangle class to represent a rectangle
+"""
+
+
+class Rectangle:
+
+    """
+    A class representing a rectangle.
+
+    Attributes:
+    Width (float): The width of the rectangle.
+    Height (float): The height of the rectangle.
+    """
