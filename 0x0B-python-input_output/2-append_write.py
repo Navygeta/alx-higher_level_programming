b28ad5c7b7b720b6e9b5d0719ca542d76ef78b92
Navygeta <tinashenavonika@gmail.com>
2023-11-07 23:35:54 +0200

    append file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Function that appends text to end of the file
+"""
+
+
+def append_file(filename="", text=""):
+    """
+    Appends contents to the end of file and returns chars to stdout
+
+    Args:
+        filename (str): File to append and  write to
+        text (str): Contents to append and write to file
+
+    Return:
+        Chars written and appended to file
+    """
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
