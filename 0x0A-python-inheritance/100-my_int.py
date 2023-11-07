ad3379f09a0d36a90e19d4fbe566a02a21055f0a
Navygeta <tinashenavonika@gmail.com>
2023-11-07 03:10:39 +0200

    my int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+class MyInt that inherits from int and modifies the behavior of
+equality operators.
+"""
+
+
+class MyInt(int):
+    """
+    Integer class that inverts the behavior of == and != operators.
+
+    Attributes:
+        value (int): The integer value.
+    """
+
+    def __eq__(self, value):
+        """
+        Overrides the == operator to implement != behavior.
+
+        Args:
+            value: The value to compare.
+
+        Returns:
+            bool: True if values are not equal, False otherwise.
+        """
+        return super().__eq__(value)
+
+    def __ne__(self, value):
+        """
+        Overrides the != operator to implement == behavior.
+
+        Args:
+            value: The value to compare.
+
+        Returns:
+            bool: True if values are equal, False otherwise.
+        """
+        return super().__ne__(value)
