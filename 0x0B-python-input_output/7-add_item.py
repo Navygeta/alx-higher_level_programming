85472db381d55865c0fb78a001ce41d23889d8b1
Navygeta <tinashenavonika@gmail.com>
2023-11-08 00:27:44 +0200

    add item

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Script that adds all arguments to a Python list and saves them to a file.
+"""
+import sys
+
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
+
+try:
+    py_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    py_list = []
+
+py_list.extend(sys.argv[1:])
+save_to_json_file(py_list, "add_item.json")
