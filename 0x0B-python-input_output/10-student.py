4a8ae54d942dae509561de04b9815e220dff02c6
Navygeta <tinashenavonika@gmail.com>
2023-11-08 01:58:33 +0200

    10 student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Representation of class Student that defines a student with attributes
+first_name, last_name, age
+"""
+
+
+class Student:
+    """
+    Representation of class student
+
+    Public instance attributes:
+        first_name
+        last_name
+        age
+
+    Public method:
+        to_json(): Retrieves a dictionary representation of a Student instance.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialization of student class
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Public method to retrieve a dict representation of student instance.
+        """
+    if isinstance(attrs, list) and all(isinstance(atr, str)
+                                       for atr in attrs):
+        return {atr: getattr(self, atr) for atr in attrs
+                if hasattr(self, atr)}
+
+    return self.__dict__
