6084fde52cbb129bfc29ebfc542896ec895ed557
Navygeta <tinashenavonika@gmail.com>
2023-11-07 23:31:28 +0200

    wrtie file2

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -15,5 +15,5 @@ def write_file(filename="", text=""):
     Return:
         Chars written to file
     """
-    with open(filename, "w" encoding="utf-8") as file:
+    with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
