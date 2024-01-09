46a3603a99a621cfe78a76b0144528eccb31a217
Navygeta <tinashenavonika@gmail.com>
2024-01-09 18:51:09 +0200

    rect #4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,20 +1,31 @@
 #!/usr/bin/node
-
 class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0) [this.width, this.height] = [w, h];
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
   }
 
-  print() {
-    Array(this.height).fill('X'.repeat(this.width)).forEach(row => console.log(row));
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
   }
 
-  rotate() {
-    [this.width, this.height] = [this.height, this.width];
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
   }
 
-  double() {
-    [this.width, this.height] = [this.width * 2, this.height * 2];
+  double () {
+    this.width *= 2;
+    this.height *= 2;
   }
 }
 
