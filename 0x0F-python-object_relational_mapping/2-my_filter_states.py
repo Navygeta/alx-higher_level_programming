3b819bc9d33c5d05591137e70e5bae9cb4109fb9
Navygeta <tinashenavonika@gmail.com>
2024-01-15 01:50:44 +0200

    fliter states two

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,13 +5,10 @@ Script that takes in an argument and displays all values in the states table of
 hbtn_0e_0_usa where name matches the argument.
 """
 
-import MySQLdb
-import sys
-
 if __name__ == "__main__":
     with MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306) as con:
         with con.cursor() as db_cursor:
-            db_cursor.execute("SELECT * FROM states WHERE name LIKE BINARY %s",
-                              (sys.argv[4],))
+            db_cursor.execute("SELECT * FROM states WHERE name LIKE BINARY %s "
+                              "ORDER BY states.id", (sys.argv[4],))
             [print(record) for record in db_cursor.fetchall()]
