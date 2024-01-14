f4d909b57f5a5cdecd591599203a499ab9878ba3
Navygeta <tinashenavonika@gmail.com>
2024-01-15 01:57:29 +0200

    2 states filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,14 +6,10 @@ hbtn_0e_0_usa where name matches the argument.
 """
 
 if __name__ == "__main__":
-    # Connecting to MySQL server
     with MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306) as con:
-        with con.cursor() as db_cursor:
-            # Creating SQL query using format and user input
-            db_cursor.execute("""SELECT * FROM states
-                                WHERE name LIKE BINARY %s
-                                ORDER BY states.id""", (sys.argv[4] + '%',))
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306) as db:
+        with db.cursor() as cur:
+            cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s",
+                        (sys.argv[4] + '%',))
 
-            # Displaying results as they are in the example
-            [print(record) for record in db_cursor.fetchall()]
+            [print(row) for row in cur.fetchall()]
