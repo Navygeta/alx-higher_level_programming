5426655d71500890ea8bc070426777743c9a7945
Navygeta <tinashenavonika@gmail.com>
2024-01-19 23:49:10 +0200

    model state corre 8

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -15,16 +15,8 @@ Base = declarative_base(metadata=mymetadata)
 class State(Base):
     """
     State class representing a state entity in the database.
-
-    Attributes:
-        __tablename__ (str): The name of the database table for
-       the 'State' class.
-       custom_id (int): An auto-generated, unique identifier for
-       each state.
-       custom_name (str): The name of the state, with a maximum
-       length of 128 characters.
-
     """
+
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
