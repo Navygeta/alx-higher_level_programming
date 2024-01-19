20a1db74204c0be2c044d2c6d516a8b5e9cfdfa5
Navygeta <tinashenavonika@gmail.com>
2024-01-19 22:45:51 +0200

    model state corr 5

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 """
 Script that contains the class definition of a State and an
 instance Base = declarative_base():
@@ -10,35 +9,22 @@ import sys
 from sqlalchemy import Column, Integer, String, create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-custom_metadata = MetaData()
+custom_meta = MetaData()
 Base = declarative_base(metadata=custom_metadata)
+
 
 class State(Base):
     """
     State class representing a state entity in the database.
 
     Attributes:
-        __tablename__ (str): The name of the database table for the 'State' class.
-        id (int): An auto-generated, unique identifier for each state.
-        name (str): The name of the state, with a maximum length of 128 characters.
+        __tablename__ (str): The name of the database table for
+       the 'State' class.
+       custom_id (int): An auto-generated, unique identifier for
+       each state.
+       custom_name (str): The name of the state, with a maximum
+       length of 128 characters.
     """
     __tablename__ = 'states'
-    custom_id = Column(Integer, unique=True, nullable=False, primary_key=True)
-    custom_name = Column(String(128), nullable=False)
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        sys.exit("Usage: {} <custom_username> <custom_password> <custom_database>"
-                 .format(sys.argv[0]))
-
-    custom_username, custom_password, custom_database = sys.argv[1:4]
-    custom_engine = create_engine(
-        f'mysql+mysqldb://{custom_username}:{custom_password}@localhost/{custom_database}',
-        pool_pre_ping=True
-    )
-
-    # Importing classes before calling create_all
-    from model_state import OtherClass  # Add any other classes you have in model_state
-
-    Base.metadata.create_all(custom_engine)
-
+    _id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    _name = Column(String(128), nullable=False)
