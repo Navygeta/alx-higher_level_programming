fe1fbcd2c87b95d7d8c281c4207999c130e199a5
Navygeta <tinashenavonika@gmail.com>
2024-01-19 23:53:37 +0200

    model state correction 9

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,18 +5,17 @@ Script that contains the class definition of a State and an
 instance Base = declarative_base():
 """
 
-from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import Column, String, Integer, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
 metadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
+Base = declarative_base(metadata=metadata)
 
 
 class State(Base):
     """
-    State class representing a state entity in the database.
+    Class with id and name attributes of each state
     """
-
-    __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    __tablename__ = "states"
+    id = Column(Integer, unique=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
