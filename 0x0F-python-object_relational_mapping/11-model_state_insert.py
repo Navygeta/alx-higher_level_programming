db3ff68c44932e05d51ece22044408364d20581d
Navygeta <tinashenavonika@gmail.com>
2024-01-20 00:38:34 +0200

    insert

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,38 +1,23 @@
 #!/usr/bin/python3
 """
-Prints the State object with the name passed as an argument from given db
+Script that prints the State object with the name passed as argument from db
 """
-
 import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
-def add_and_print_state(username, password, database, new_state_name):
-    """Add a new state to the database and print its ID."""
-    engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}'
-    )
-    Base.metadata.create_all(engine)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    new_state = State(name=new_state_name)
+    new_state = State(name='Louisiana')
     session.add(new_state)
-    session.commit()
+    new_instance = session.query(State).filter_by(name='Louisiana').first()
 
-    new_instance = session.query(State).filter_by(name=new_state_name).first()
     print(new_instance.id)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        sys.exit(
-            "Usage: {} <username> <password> <database> <new_state_name>"
-            .format(sys.argv[0])
-        )
-
-    username, password, database, new_state_name = sys.argv[1:5]
-    add_and_print_state(username, password, database, new_state_name)
+    session.commit()
