4fef866c0fb47ce963ed9593586c8a84434eeea6
Navygeta <tinashenavonika@gmail.com>
2024-01-27 01:57:18 +0200

    status 3

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,19 +5,15 @@ SCript that Fetches https://intranet.hbtn.io/status.
 
 import urllib.request
 
-
-def fetch_url_content(url):
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        return body
-
-
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    content = fetch_url_content(url)
+    target_url = "https://intranet.hbtn.io/status"
 
-    print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
-    print("\t- utf8 content: {}".format(content.decode("utf-8")))
+    url_request = urllib.request.Request(target_url)
+
+    with urllib.request.urlopen(url_request) as url_response:
+        response_body = url_response.read()
+
+        print("Body response:")
+        print("\t- type: {}".format(type(response_body)))
+        print("\t- content: {}".format(response_body))
+        print("\t- utf8 content: {}".format(response_body.decode("utf-8")))
