515ebf33401d9f2619beb2a253eae9f28fbe576a
Navygeta <tinashenavonika@gmail.com>
2024-01-27 01:52:01 +0200

    status 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,11 +5,17 @@ SCript that Fetches https://intranet.hbtn.io/status.
 
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
+def fetch_url_content(url):
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        return body
 
-with urllib.request.urlopen(url) as response:
-    content = response.read().decode('utf-8')
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    content = fetch_url_content(url)
 
     print("Body response:")
     print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content)
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(content.decode("utf-8")))
