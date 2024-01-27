ea4567003fda4d2d7442e07030c3b7e483e406f9
Navygeta <tinashenavonika@gmail.com>
2024-01-27 02:22:04 +0200

    status 6

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         content = response.read()
 
-        print("Body response:$")
-        print("\t- type: {}".format(type(content)) + "$")
-        print("\t- content: {}".format(repr(content)) + "$")
-        print("\t- utf8 content: {}".format(content.decode('utf-8')) + "$")
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(repr(content)))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
