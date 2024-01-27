d6d97feabc84e9493feb802c1d016ec5f87701af
Navygeta <tinashenavonika@gmail.com>
2024-01-27 03:01:52 +0200

    error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL
+and displays the body of the response.
+"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+
+    print("Error code: {}".format(response.status_code)) \
+        if response.status_code >= 400 else print(response.text)
