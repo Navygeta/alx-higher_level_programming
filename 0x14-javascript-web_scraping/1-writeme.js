6a4d27f5201aaa59bb56fd96f9c94e246300fcbf
Navygeta <tinashenavonika@gmail.com>
2024-02-21 01:21:47 +0200

    my file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
 'use strict';
-require('fs').promises.writeFile(process.argv[2], process.argv[3], 'utf-8')
-  .then(() => console.log(`Content written to ${process.argv[2]}`))
-  .catch(error => console.error(error));
+require('fs').promises.writeFile(process.argv[2], process.argv[3], 'utf-8').catch(console.error);
